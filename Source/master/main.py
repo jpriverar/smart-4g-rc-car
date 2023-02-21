2dc8431190690c26adc80defc1c955816a767239
Juan Pablo Rivera Ruiz <rivera.ruiz.juanp@gmail.com>
2023-02-20 23:25:36 -0600

    First approach to communication protocol

--- Source/master/main.py
+++ Source/master/main.py
@@ -2,6 +2,34 @@ import RPi.GPIO as GPIO
 import utils
 from uart_messenger import UART_Messenger
 import threading
+import time
+
+def car_setup():
+    # Sending a reset_signal to the arduino
+    print("Resetting slave")
+    ret = slave.send_reset()
+    if not ret:
+        print("Something went wrong, exitting...")
+        return False
+
+    # Waiting till connection is stablished
+    print("Attempting UART connection with slave")
+    ret = slave.wait_for_connection(timeout=5)
+    if not ret:
+        print("Timeout reached, could not stablish connection with slave!")
+        return False
+        
+    # Waiting for the slave to initialize all car components
+    print("Waiting for all components to be initialized by slave...")
+    ret = slave.wait_for_message("ready", timeout=10)
+    if not ret:
+        print("Timeout reached, looks like some components are not initializing...")
+        return False
+        
+    time.sleep(3)
+    slave.flushInput()
+    print("Setup successfull! Slave ready to receive commands...")
+    return True
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -10,19 +38,15 @@ GPIO.setup(reset_pin, GPIO.OUT)
 
 # Creating arduino comms instance
 slave = UART_Messenger('/dev/ttyAMA1', 115200, timeout=1, reset_pin=reset_pin)
-
-# Sending a reset_signal to the arduino
-print("Waiting before reset")
-status = slave.send_reset()
-if status != 0: exit()
-
-# Waiting till connection is stablished
-status = slave.wait_for_connection()
-if status != 0: exit()
-
-# Unit testing the components
-x = threading.Thread(target=utils.steering_test, args=(slave,))
-x.start()
+if not car_setup(): exit()
+    
+#slave.send_msg("SS0")
+#time.sleep(2)
+#slave.send_msg("SS200")
+#time.sleep(2)
+#slave.send_msg("SC")
+#time.sleep(2)
+slave.send_msg("FC")
 
 while True:
     slave.fetch_msg()
--- Source/master/uart_messenger.py
+++ Source/master/uart_messenger.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import serial
 import time
 from collections import deque
+import struct
 
 class UART_Messenger(serial.Serial):
     def __init__(self, device, baud_rate, timeout, reset_pin):
@@ -16,48 +17,94 @@ class UART_Messenger(serial.Serial):
             GPIO.output(self.reset_pin, 1)
             time.sleep(0.1)
             GPIO.output(self.reset_pin, 0)
-            print("Reset sent...")
-            return 0
+            return True
+        
         except Exception as e:
-            print("Could not send reset signal: " + str(e))
-            return 1
+            print(e)
+            return False
     
     def send_msg(self, msg):
         try:
             msg = msg + "\n"
             msg = msg.encode()
             self.write(msg)
-            return 0
-        except:
-            print("Could not send msg, please check it is UTF-8 encodable")
-            return 1
+            return True
+        
+        except Exception as e:
+            print(e)
+            return False
         
     def fetch_msg(self):
-        try:
-            if self.in_waiting > 0:
-                line = self.readline().decode('utf-8').rstrip()
-                self.msg_queue.append(line)
-                print(line)
-        except Exception as e:
-            print("Could not fetch message: " + str(e))
-        
-    def wait_for_connection(self, timeout=3):
-        print("Attempting connection with arduino")
+        #try:
+        if self.in_waiting > 0: #Header for each message is 3 bytes
+            msg_header = self.read(3)
+            msg_type, payload_length = struct.unpack("<BH", msg_header)
+            print(hex(msg_type), hex(payload_length))
+            
+            if msg_type == 0x01: # Ultrasonic sensor measurement
+                sensor_data = struct.unpack("<Bf", self.read(payload_length))
+                side, distance = sensor_data
+                print(f"{side}: {distance}")
+                
+            elif msg_type == 0x02: # IMU measurement
+                sensor_data = struct.unpack("<6f", self.read(payload_length))
+                yaw, pitch, roll, ax, ay, az = sensor_data
+                print(f"ypr: {yaw}, {pitch}, {roll} acc: {ax}, {ay}, {az}")
+                    
+                #line = self.readline().decode('utf-8').rstrip()
+                #self.msg_queue.append(line)
+                
+        #except Exception as e:
+            #print("Could not fetch message: " + str(e))
+                
+    def wait_for_message(self, msg, timeout):
         start_time = time.time()
         curr_time = time.time()
         
         while (curr_time - start_time) < timeout:
-            # If a message is available in the buffer
             if self.in_waiting > 0:
                 try:
+                    # Reading available messages in the input buffer
                     line = self.readline().decode('utf-8').rstrip()
-                    if line == "ready":
-                        self.send_msg("OK")
-                        print("Connection successful!")
-                    return 0
-                except:
-                    print("Received bad formatted message, skipping...")
+                    print(line)
+                    if line == msg:
+                        return True
+                    
+                except Exception as e:
+                    print("Bad message: " + str(e))
                     
             curr_time = time.time()
-        print("Connection timeout...")
-        return 1
+        return False
+        
+    def wait_for_connection(self, timeout=10):
+        if self.wait_for_message("available", timeout=timeout):
+            if self.send_msg("OK"):
+                return True
+        return False
+            
+
+
+if __name__ == "__main__":
+    # GPIO setup
+    GPIO.setmode(GPIO.BCM)
+    reset_pin = 23
+    GPIO.setup(reset_pin, GPIO.OUT)
+
+    # Creating arduino comms instance
+    slave = UART_Messenger('/dev/ttyAMA1', 115200, timeout=1, reset_pin=reset_pin)
+
+    # Sending a reset_signal to the arduino
+    print("Resetting slave")
+    ret = slave.send_reset()
+    if not ret:
+        print("Something went wrong, exitting...")
+        exit()
+
+    # Waiting till connection is stablished
+    print("Attempting UART connection with slave")
+    ret = slave.wait_for_connection(timeout=5)
+    if not ret:
+        print("Timeout reached, could not stablish connection with slave!")
+        exit()
+        
+    print("Setup successfull! Slave ready to receive commands...")
